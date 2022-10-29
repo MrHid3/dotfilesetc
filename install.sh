4e6fa398d1852b2b24860ee9a1c13dc0aa5d3716
Laptop dom <gkornas12@gmail.com>
2022-10-29 08:13:40 +0000

    i3bar

--- install.sh
+++ install.sh
@@ -25,23 +25,23 @@ fi
 echo "Initiating configuration installation..."
 
 echo "Copying .xinitrc to $homepath"
-cp configs/.xinitrc $homepath
+cp configs/.xinitrc $homepath/
 echo ".xinitrc (X server configuration file) copied succesfully"
 
 echo "Copying .alacritty.yml to $homepath"
-cp configs/.alacritty.yml $homepath
+cp configs/.alacritty.yml $homepath/
 echo ".alacritty.yml (Alacritty configuration file) copied succesfully!"
 
 echo "Copying .bashrc to to $homepath"
-cp configs/.bashrc $homepath
+cp configs/.bashrc $homepath/
 echo ".bashrc copied succesfully!"
 
 echo "Copying .bash_profile to $homepath"
-cp configs/.bash_profile $homepath
+cp configs/.bash_profile $homepath/
 echo ".bash_profile copied succesfully!"
 
 echo "Copying picom.conf to $homepath/.config"
-cp configs/picom.conf $homepath
+cp configs/picom.conf $homepath/
 echo "picom.conf (X11/ compositor configuration file) copied succesfully!"
 
 echo "Copying wallpaper.jpg to $homepath/Pictures"
@@ -49,7 +49,7 @@ if [ ! -d $homepath/Pictures ]; then
 	mkdir $homepath/Pictures
 	echo "Created directory $homepath/Pictures"
 fi
-cp pictures/wallpaper.jpg $homepath/Pictures
+cp pictures/wallpaper.jpg $homepath/Pictures/
 echo "wallpaper.jpg copied succesfully!"
 
 echo "Copying config to $homepath/.config/i3"
@@ -61,19 +61,18 @@ if [ ! -d "$homepath/.config/i3" ]; then
 	mkdir $homepath/.config/i3
 	echo "Created directory $homepath/.config/i3"
 fi
-cp configs/config $homepath/.config/i3
+cp configs/config $homepath/.config/i3/
 echo "config (i3 configuration file) copied succesfully!"
-cp configs/config $homepath/.config/i3
 
-echo "Copying i3status.conf to $homepath/.congih/i3status"
-cp configs/i3status.conf  $homepath/.config
-echo "i3status/config (i3bar configuration file) copied succesfully!"
+echo "Copying .i3status.conf to $homepath"
+cp configs/.i3status.conf  $homepath/
+echo ".i3status.config (i3bar configuration file) copied succesfully!"
 
 read -p "Do you want to modify your grub config? [y/n]" yn
 
 if [ $yn == "y" ]; then
 echo "Copying grub to /etc/default/grub"
-sudo cp configs/grub /etc/default/grub
+sudo cp configs/grub /etc/default/
 echo "grub (grub configuration file) copied succesfully!"
 echo "Updating grub..."
 sudo update-grub
